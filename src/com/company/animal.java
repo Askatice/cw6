0853681e148ce1370085472bd795359e9ec3ac93
Askat Iliasbekov <97965896+Askatice@users.noreply.github.com>
2022-02-02 14:52:40 +0600

    Initial commit

---
+++ src/com/company/animal.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class animal {
+    private String color, breed;
+    private int age;
+
+    public animal(String color, String breed, int age) {
+        this.color = color;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/com/company/cat.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class cat extends animal{
+
+    public cat(String color, String breed, int age) {
+        super(color, breed, age);
+    }
+}
